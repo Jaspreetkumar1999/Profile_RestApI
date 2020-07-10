e26febd4aa2230855eb241d46b529210edca5f9d
Jaspreetkumar1999 <jamesjassi04@gmail.com>
2020-07-10 19:47:35 +0530

    Added filter

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from . import models
 from . import permission
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -107,6 +108,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
      queryset = models.UserProfile.objects.all()
      authentication_classes = {TokenAuthentication,}
      permission_classes = {permission.UpdateOwnProfile,}
+     filter_backends = {filters.SearchFilter,}
+     search_fields = {'name','email',}
 
 
 
